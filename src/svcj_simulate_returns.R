58dadf4c14f6015934071b4e10c2810131129cda
Julian <julian.winkel@hu-berlin.de>
2021-02-19 14:21:20 +0100

    svcj physical density

--- src/svcj_simulate_returns.R
+++ src/svcj_simulate_returns.R
@@ -19,6 +19,83 @@ rm(lib)
 # Get svcj function
 source("src/svcj_model.R")
 
+
+
+svcj_physical = function(mu, mu_y, sigma_y, lambda, alpha, beta, rho, sigma_v, rho_j, mu_v, kappa, theta, n){
+    # Simulate Physical Density as in 
+    # SVCJ Option App by Ivan Perez and Change of Measure Script
+
+    # Create Matrices to collect output
+    V    = matrix(0, nrow = n, ncol  = 1)  # Volatility of log return
+    Y    = matrix(0, nrow = n, ncol  = 1)  # Log return
+    #Jv   = matrix(0, nrow = iter, ncol  = n)  # Jumps in volatility
+    #Jy   = matrix(0, nrow = iter, ncol  = n)  # Jumps in log return
+    V[1,1] = mu_v  # Initial value of volatility = mean of volatilty
+
+    # Const Helpers
+    rho_sigma_prod = rho * sigma_v
+    sigma_v_sq = sigma_v^2
+
+    # Calculate RVs as vectors
+    
+    # Notation:
+    # Subscript y or s for stock, v for vola
+    # time indices are omitted
+    
+    # Jump Indicator
+    J_s = rbern(n = n, prob = lambda)  # Bernoulli distributed random value with lambda = 0.051 for determining whether a jump exists
+    J_v = rbern(n = n, prob = lambda)
+
+    # Jumps Sizes
+    Z_v       = rexp(n = n, rate = 1/mu_v)  # Exponential distributed random value jump size in volatility
+    Z_y       = rnorm(n = n, mean = mu_y + rho_j * mu_v, sd = sigma_y)  # Jump size of log return
+    
+    # Expectation of Jump in returns
+    E_gamma = mean(Z_y)
+
+    # Errors
+    epsilon_mat = mvrnorm(n = n, mu = c(0,0), Sigma = matrix(c(1, rho_sigma_prod, rho_sigma_prod, sigma_v_sq), nrow = 2))
+    
+
+    # 1 day periods
+    Delta = 1/365 # Choose Delta according to Belaygorod05, Chapter 3.2
+    #print('@Todo: Muessen die Delta subscripts noch runterskaliert werden auf Tagesbasis? E.g. mu = mu/365')
+    # Problem with Euler Scheme: Vola can be negative for large Deltas
+    for(i in 2:n){
+
+        epsilon_s = try(epsilon_mat[i,1])
+        if('try-error' %in% class(epsilon_s)){
+            print(str(epsilon_s))
+            print(i)
+        }
+        
+        epsilon_v = epsilon_mat[i,2]
+        
+        # Scalars
+        #mu_bar = (exp(mu_y + 0.5 * sigma_y) / (1 - mu_y * rho_j)) - 1
+
+        # Calculate log diff in Stock price and Vola diff
+        d_log_s = (mu - lambda * E_gamma) * Delta + sqrt(V[i-1, 1] * Delta) * epsilon_s + Z_y[i] * J_s[i]
+        d_v     = kappa * (theta - V[i-1,1]) * Delta + sqrt(V[i-1, 1] * Delta) * epsilon_v + Z_v[i] * J_v[i]
+    
+        # Collect results
+        Y[i,1] = d_log_s
+        
+        # If Vola is nonnegative add, else return to mean:
+        vola_i = V[i-1, 1] + d_v
+        if(vola_i > 0){
+            V[i,1] = vola_i
+        }else{
+            print('vola would be negative, reverting to mean...')
+            V[i,1] = mu_v
+        }
+
+        #print(vola_i)
+    # Just lapply this n times and process in apply function below :)
+    }
+    return(unlist(Y)) # log diff prices
+}
+
 execute_svcj_sim = function(ndays = 10, n = 2000, iter = 5000, startdate = as.Date('2020-01-01'), startvalue = 0L, do_plot = TRUE, prices = NULL, cached_svcj_parameters = FALSE){
 
     # @ Todo: this is failing in main proabably because n is too small!
@@ -29,7 +106,7 @@ execute_svcj_sim = function(ndays = 10, n = 2000, iter = 5000, startdate = as.Da
     #tdat = read.csv('data/BTCUSDT.csv', stringsAsFactors = FALSE)
     if(is.null(prices)){
         print('loading btc returns')
-        btc_price_time_series_file = 'data/BTCUSDT.csv'
+        btc_price_time_series_file = 'data/BTC_USD_Quandl.csv'
         tdat = read.csv(btc_price_time_series_file, stringsAsFactors = FALSE)
         prices = data.frame(list('Adj.Close' = tdat$Adj.Close,
                             'date' = as.Date(tdat$Date)))
@@ -59,125 +136,46 @@ execute_svcj_sim = function(ndays = 10, n = 2000, iter = 5000, startdate = as.Da
 
 
     # --------------------------------------
-    # Price Simulation using SVCJ parameters
+    # Physical Density Simulation
     # --------------------------------------
 
-    #cryptos <- c("btc")
     crypto = 'btc'
     svcj_results = list('btc' = btc_svcj)
 
     simulated_returns_long = list()
-    parameters = svcj_results[[crypto]]$parameters
-    mu = parameters[1,2]
-    mu_y = parameters[2,2]  
-    sigma_y = parameters[3,2]  
-    lambda = parameters[4,2]
-    alpha = parameters[5,2]
-    beta = parameters[6,2]
-    rho = parameters[7,2]
-    sigma_v = parameters[8,2]
-    rho_j = parameters[9,2]
-    mu_v = parameters[10,2]
-    kappa = -(alpha/beta)
-    theta = alpha/kappa
-    
-    # Create empty vectors to store the simulated values
-    V    = matrix(0, nrow = iter, ncol  = n)  # Volatility of log return
-    Y    = matrix(0, nrow = iter, ncol  = n)  # Log return
-    Jv   = matrix(0, nrow = iter, ncol  = n)  # Jumps in volatility
-    Jy   = matrix(0, nrow = iter, ncol  = n)  # Jumps in log return
-    V[,1] = mu_v  # Initial value of volatility = mean of volatilty
-    
-        for (i in 2:n) {
-        Z = mvrnorm(n = iter, mu = c(0,0), Sigma = matrix(c(1,rho,rho,1), nrow = 2))  # Standard normal random value
-        Z1 = Z[,1]
-        Z2 = Z[,2] 
-        J = rbern(n = iter, prob = lambda)  # Bernoulli distributed random value with lambda = 0.051 for determining whether a jump exists
-        XV       = rexp(n = iter, rate = 1/mu_v)  # Exponential distributed random value with mV = 0.709 for jump size in volatility
-        X        = rnorm(n = iter, mean = mu_y + rho_j * XV, sd = sigma_y)  # Jump size of log return
-        #V[i]     = alpha + beta*V[i-1] + sigma_v*sqrt(V[i-1])*Z2 + XV*J  # Volatilty
-        V[,i]     = kappa * theta + (1 - kappa) * V[,i- 1] + sigma_v*sqrt(V[,i-1])*Z2 + XV*J
-        Y[,i]     = mu + sqrt(V[,i-1])*Z1 + X*J  # Log return
-        Jv[,i]    = XV*J  # Jumps in volatilty (0 in case of no jump)
-        Jy[,i]    = X*J  # Jumps in log return (0 in case of no jump)
-        #print(paste0("Simulation ",i," of ", n, " for ", crypto))
-        }
-    
-    simulated_returns_long[[crypto]] <- setNames(list(Y, V), 
-                                                c("simulated_returns", "simulated_volatility"))
-        
-    #}
-    
+    parameters          = svcj_results[[crypto]]$parameters
+    mu                  = parameters[1,2]
+    mu_y                = parameters[2,2]  
+    sigma_y             = parameters[3,2]  
+    lambda              = parameters[4,2]
+    alpha               = parameters[5,2]
+    beta                = parameters[6,2]
+    rho                 = parameters[7,2]
+    sigma_v             = parameters[8,2]
+    rho_j               = parameters[9,2]
+    mu_v                = parameters[10,2]
+    kappa               = -(alpha/beta)
+    theta               = alpha/kappa
 
-    #rm(parameters, alpha, beta, crypto, i, J, 
-    #Jv, Jy, kappa, lambda, mu, mu_v, mu_y, rho, rho_j, sigma_v, sigma_y,
-    #theta, V, X, XV, Y, Z1, Z2, Z)
-
-    #save(simulated_returns_long, file = "simulated_returns_long.Rda")
-    #load("simulated_returns_long.Rda")
-
-    # Store a smaller list of simulated returns (to save memory space for the app) 
-    #simulated_returns <- list()
-    #for (i in cryptos) {
-    #simulated_returns[[i]] <- simulated_returns_long[[i]]$simulated_returns[,1:361]
-    #}
-
-
-    # Plot some simulated price paths for ETH
-    #if(startvalue == 0L){
-    #print('using startvalue from btc time series')
     init_price <- prices$Adj.Close[prices$date == startdate] 
     print(paste0('init price in svcj sim: ', init_price))
-    #}else{
-    #    init_price = startvalue
-    #}
-    sim_price <- data.frame(matrix(NA, nrow = iter, ncol = n))
-    sim_price[,1] <- init_price
 
-    # Revert from returns back to prices
-    for (i in 2:n) {
-        #print(i)
-    sim_price[,i] <- (simulated_returns_long$btc$simulated_returns[,i]/sqrt(250) + 1)*sim_price[,i-1] 
-    }
-    
-    # @Todo need to restrict sim_price to the correct amount of days here
-    out = apply(sim_price, 1, function(z, .ndays = ndays){
-        raw = na.omit(z)
-        f = raw[1] #head(raw, 1)
-        l = raw[.ndays] #tail(raw, 1)
-        ret = (l-f)/f
-        return(ret)
+    out = lapply(1:iter, function(z, startprice = init_price){
+        sim_prices = svcj_physical(mu, mu_y, sigma_y, lambda, alpha, beta, rho, sigma_v, rho_j, mu_v, kappa, theta, ndays)
+        #browser()
+        #@Todo make sure overall_rets cant be crazier than a loss of 1
+        overall_rets = (cumprod(1+sim_prices) - 1)
+        lastprice  = startprice + (startprice * tail(overall_rets, 1))
+        rets       = (lastprice - startprice)/startprice
+        #compounded_rets = rets*ndays/365
+        #print('@Todo: Check compounded rets')
+        return(rets)
     })
     #browser()
-    if(do_plot){
-
-        # Plot density too
-
-        # Plot simulated return for BTC
-        ggplot() + 
-        geom_line(aes(x = seq(1:1000), y = simulated_returns_long$Adj.Close$simulated_returns[100,]), col = "black") + 
-        geom_line(aes(x = seq(1:1000), y = simulated_returns_long$Adj.Close$simulated_returns[1000,]), col = "blue") + 
-        labs(ylab("Daily Returns")) +
-        labs(xlab("Days")) +
-        theme_bw() +
-        theme(panel.grid = element_blank()) 
-
-        # Plot some simulated price paths for ETH
-        ggplot() + 
-        geom_line(aes(x = seq(1:1000), y = unlist(sim_price[500,])), col = "black") + 
-        geom_line(aes(x = seq(1:1000), y = unlist(sim_price[1000,])), col = "blue") + 
-        geom_line(aes(x = seq(1:1000), y = unlist(sim_price[2501,])), col = "green") +
-        geom_line(aes(x = seq(1:1000), y = unlist(sim_price[3500,])), col = "red") +
-        geom_line(aes(x = seq(1:1000), y = unlist(sim_price[5000,])), col = "purple") +
-        labs(ylab("Daily Returns")) +
-        labs(xlab("Days")) +
-        theme_bw() +
-        theme(panel.grid = element_blank()) 
-    }
-
+    
     # Return list of simulated returns
     return(out)
 
 }
 
-#execute_svcj_sim(ndays = 10, n = 1000, iter = 5000, startdate = as.Date('2020-01-01'), startvalue = 0L, do_plot = FALSE, prices = NULL)
+#o = execute_svcj_sim(ndays = 10, n = 5000, iter = 5000, startdate = as.Date('2020-01-01'), startvalue = 0L, do_plot = FALSE, prices = NULL, cached_svcj_parameters = TRUE)
